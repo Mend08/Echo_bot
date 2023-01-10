9a06b76185f74cb2774ccbe110233097ffb60a23
Александр <alex.menkenov@yandex.ru>
2023-01-10 16:37:06 +0300

    Telegram_bot

--- echo_bot.py
+++ echo_bot.py
@@ -7,7 +7,7 @@ def welcome(message):
 	sti = open('static/welcome.webp', 'rb')
 	bot.send_sticker(message.chat.id, sti)
 
-	bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный Хоней на Python.".format(message.from_user, bot.get_me()), 
+	bot.send_message(message.chat.id, "Привет, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный Хоней на Python.".format(message.from_user, bot.get_me()), 
 		parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
